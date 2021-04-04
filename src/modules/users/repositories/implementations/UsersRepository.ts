6398ded232402a165d80098f598773c2c929ceba
joselucas94 <55204225+joselucas94@users.noreply.github.com>
2021-04-04 07:37:25 -0300

    Update UsersRepository.ts

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,29 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne(user_id, {relations:["games"]});
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+
+    const users = await this.repository.query( "SELECT * FROM users ORDER BY first_name ASC " )
+
+
+    return users;
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+
+    const llast_name =  last_name.toLowerCase();
+    const lfirst_name = first_name.toLowerCase();
+       
+       const users = await this.repository.query("SELECT * FROM users WHERE lower(first_name) = $1 AND lower(last_name) = $2", [lfirst_name, llast_name])
+      
+       return users;
   }
 }
